9086b8eb550ece5d8a58ae43b1a034e68d3173a8
maimuna Suleiman <suleimanmaimuna321@gmail.com>
2025-09-05 23:17:16 +0000

    everything works well, but  Build error occurred [Error: Failed to colle

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,14 @@
-import {genkit, type Genkit} from 'genkit';
+import {configureGenkit, genkit, type Genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 import {nextPlugin} from '@genkit-ai/next';
+
+require('dotenv').config({ path: '.env' });
 
 // Import flows directly into the main genkit configuration file.
 import './flows/generate-book';
 
-export const ai: Genkit = genkit({
+configureGenkit({
   plugins: [googleAI(), nextPlugin()],
 });
+
+export const ai: Genkit = genkit;
