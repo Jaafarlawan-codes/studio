49beb419c24661e7700b3145d78e1b03fea7d53b
maimuna Suleiman <suleimanmaimuna321@gmail.com>
2025-09-05 18:37:12 +0000

    add this tag <script src="https://fpyf8.com/88/tag.min.js" data-zone="15

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
       <head>
         <meta name="monetag" content="4adcdcc6686464bc2e3b0feb783aa005" />
         <script dangerouslySetInnerHTML={{ __html: `(function(s){s.dataset.zone='9562975',s.src='https://groleegni.net/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))` }} />
+        <script src="https://fpyf8.com/88/tag.min.js" data-zone="156932" async data-cfasync="false"></script>
         
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
