90dbe8fa6da86a1918d308e64796c6253f62cd44
maimuna Suleiman <suleimanmaimuna321@gmail.com>
2025-09-05 23:13:16 +0000

    Error: ./src/ai/genkit.ts:4:1 Export [32mnext[39m doesn't exist in tar

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,11 +1,10 @@
-require('dotenv').config();
 import {genkit, type Genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {next} from '@genkit-ai/next';
+import {nextPlugin} from '@genkit-ai/next';
 
 // Import flows directly into the main genkit configuration file.
 import './flows/generate-book';
 
 export const ai: Genkit = genkit({
-  plugins: [googleAI(), next()],
+  plugins: [googleAI(), nextPlugin()],
 });
